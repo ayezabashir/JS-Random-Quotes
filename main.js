1f79fc58037f60b89d6925db8311a178fe14e67b
Ayeza <ayezabashir442@gmail.com>
2023-08-18 00:19:48 +0500

    getting data from API

---
+++ main.js
@@ -0,0 +1,18 @@
+let content = document.querySelector('.content');
+let btn = document.getElementById('btn');
+let url = 'https://api.quotable.io/random';
+
+btn.addEventListener('click', getQuoute)
+
+function getQuoute() {
+    fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+            content.innerHTML = `
+        <q id="quote">${data.content}</q>
+        <h3 id="author"> ${data.author} </h3>
+        `
+        });
+}
+
+getQuoute();
